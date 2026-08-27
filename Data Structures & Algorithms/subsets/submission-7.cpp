e705ba1581fd035ae65560ddbf2949385757f9a3
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-08-27 12:15:20 -0400

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res = {{}};
+        for (auto num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                vector<int> cur = res[i];
+                cur.push_back(num);
+                res.push_back(cur);
+            }
+        }
+        return res;
+    }
+};
