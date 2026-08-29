11aacc756cd6e2bcff70616aeb68e1cb63bb3713
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-08-29 11:47:09 -0400

    Add: search-for-word - submission-5

---
+++ Data Structures & Algorithms/search-for-word/submission-5.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    vector<vector<bool>> visited;
+    bool res = false;
+    vector<vector<char>> board;
+    string word;
+    void dfs(int i, int j, int k) {
+        if (k == word.size() or i < 0 or i >= board.size() or j < 0 or j >= board[0].size() or visited[i][j])
+            return;
+        if ((k == word.size() - 1) and (board[i][j] == word[k])) {
+            res = true;
+            return;
+        }
+        visited[i][j] = true;
+        dfs(i + 1, j, k + 1);
+        dfs(i - 1, j, k + 1);
+        dfs(i, j + 1, k + 1);
+        dfs(i, j - 1, k + 1);
+        visited[i][j] = false;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        visited = vector<vector<bool>> (board.size(), vector<bool>(board[0].size(), false));
+        this->board = board;
+        this->word = word;
+        
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (board[i][j] == word[0]) {
+                    dfs(i, j, 0);
+                    if (res == true)
+                        break;
+                }
+            }
+        }
+        return res;
+    }
+};
