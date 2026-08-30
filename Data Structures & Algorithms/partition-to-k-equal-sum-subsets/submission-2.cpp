2385aac446a196f0e2ad28c9697836aee7872413
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-08-29 22:31:39 -0400

    Add: partition-to-k-equal-sum-subsets - submission-2

---
+++ Data Structures & Algorithms/partition-to-k-equal-sum-subsets/submission-2.cpp
@@ -0,0 +1,42 @@
+class Solution {
+private:
+    int sum = 0;
+    bool res = false;
+    vector<int> nums;
+    int k;
+
+    void dfs(int i, int cur) {
+        if (cur > sum)
+            return;
+        if (i == k - 1) {
+            res = true;
+            return;
+        }
+        if (cur == sum) {
+            dfs(i + 1, 0);
+            return;
+        }    
+        for (auto num : nums) {
+            if (cur + num > sum)
+                continue;
+            else {
+                dfs(i, cur + num);
+                if (res)
+                    return;
+            }
+        }
+    }
+public:
+    bool canPartitionKSubsets(vector<int>& nums, int k) {
+        this->nums = nums;
+        this->k = k;
+        sort(nums.begin(), nums.end());
+        for (auto num : nums)
+            sum += num;
+        if (sum % k != 0)
+            return false;
+        sum /= k;
+        dfs(0, 0);
+        return res;
+    }
+};
