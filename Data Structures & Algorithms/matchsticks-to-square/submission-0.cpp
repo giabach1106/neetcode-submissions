1fc9c03edd767b378723d35c396549fc52827c1b
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-08-29 22:11:41 -0400

    Add: matchsticks-to-square - submission-0

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-0.cpp
@@ -0,0 +1,37 @@
+class Solution {
+private:    
+    bool res = false;
+    int sum;
+    void dfs(int i, int cur, map<int, int>& m) {
+        if (cur > sum)
+            return;
+        if (i == 3) {
+            res = true;
+            return;
+        }
+        if (cur == sum) {
+            dfs(i + 1, 0, m);
+            return;
+        }
+        for (auto& [x, y] : m) {
+            if (y == 0)
+                continue;
+            if (cur + x <= sum) {
+                y--;
+                dfs(i, cur + x, m);
+                y++;
+            }
+        }
+    }
+public:
+    bool makesquare(vector<int>& matchsticks) {
+        map<int, int> m;
+        for (auto matchstick : matchsticks) {
+            sum += matchstick;
+            m[matchstick]++;
+        }
+        sum /= 4;
+        dfs(0, 0, m);
+        return res;
+    }
+};
