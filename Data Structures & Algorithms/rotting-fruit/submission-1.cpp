aced89ac9e49dd94d2d1b2073b4f3e2667dab423
Bach Le <95017142+giabach1106@users.noreply.github.com>
2026-09-04 22:05:13 +0700

    Add: rotting-fruit - submission-1

---
+++ Data Structures & Algorithms/rotting-fruit/submission-1.cpp
@@ -0,0 +1,38 @@
+class Solution {
+private:
+    bool check (int i, int j, vector<vector<int>>& grid) {
+        if (i < 0 or j < 0 or i >= grid.size() or j >= grid[i].size() or grid[i][j] != 1)
+            return false;
+        grid[i][j] = 2;
+        return true;
+    }
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int count = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 2) {
+                    queue<pair<int, int>> q;
+                    q.push({i, j});
+                    while (!q.empty()) {
+                        int size = q.size();
+                        for (int i = 0; i < size; i++) {
+                            auto [x, y] = q.front();
+                            q.pop();
+                            if (check(x + 1, y, grid)) q.push({i + 1, j});
+                            if (check(x - 1, y, grid)) q.push({i - 1, j});
+                            if (check(x, y - 1, grid)) q.push({i, j - 1});
+                            if (check(x, y + 1, grid)) q.push({i, j + 1});
+                        }
+                        count++;
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < grid.size(); i++)
+            for (int j = 0; j < grid[i].size(); j++)
+                if (grid[i][j] == 1)
+                    return -1;
+        return count;
+    }
+};
